3bd8ced4ae9f58630b342726919b7e5f328214d1
Nicholson Galela <nicholsong@saica.co.za>
2022-03-18 15:52:50 +0200

    develop AppShell folder, fix Drawer.jsx component

--- mtn-app/src/containers/AppShell/App.jsx
+++ mtn-app/src/containers/AppShell/App.jsx
@@ -1,12 +1,13 @@
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import React, { Component } from 'react';
+import Drawer from './Drawer';
 
 class App extends Component {
   render() {
     // global theme for defining the company colors and font colors 
     const { theme } = this.props
     return(<MuiThemeProvider theme={theme}>
-      test
+      <Drawer />
     </MuiThemeProvider>)
   }
 }
---
+++ mtn-app/src/containers/AppShell/Drawer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Drawer extends Component {
+  render() {
+    const { theme } = this.props
+    return(<>test</>)
+  }
+}
+
+export default Drawer;
