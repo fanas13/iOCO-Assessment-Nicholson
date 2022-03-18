4f45dc0e3fa34df704c99f68a0df25c45c2e93b3
Nicholson Galela <nicholsong@saica.co.za>
2022-03-18 15:56:00 +0200

    created two components Cards.jsx and Carousel.jsx to render the carousel

---
+++ mtn-app/src/routes/FeatureRoutes/Carousel/Cards,jsx.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Cards extends Component {
+  render() {
+    const { theme } = this.props
+    return(<>test</>)
+  }
+}
+
+export default Cards;
---
+++ mtn-app/src/routes/FeatureRoutes/Carousel/Carousel.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+  render() {
+    const { theme } = this.props
+    return(<>test</>)
+  }
+}
+
+export default Carousel;
